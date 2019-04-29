959f13435bc694fb0304ea5760b709412c807f46
hmarcone <hmarcone@gmail.com>
2019-04-29 10:28:02 -0300

    Add files via upload

---
+++ Reply.UpLoad/Interfaces/IFormFile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reply.UpLoad.Interfaces
+{
+    public interface IFormFile
+    {
+        // Gets the raw Content-Type header of the uploaded file.
+        string ContentType { get; }
+
+        // Gets the raw Content-Disposition header of the uploaded file.
+        string ContentDisposition { get; }
+
+        // Gets the header dictionary of the uploaded file.
+        IHeaderDictionary Headers { get; }
+
+        // Gets the file length in bytes.
+        long Length { get; }
+
+        // Gets the name from the Content-Disposition header.
+        string Name { get; }
+
+        // Gets the file name from the Content-Disposition header.
+        string FileName { get; }
+
+        // Copies the contents of the uploaded file to the target stream.
+        void CopyTo(Stream target);
+
+        // Asynchronously copies the contents of the uploaded file to the target stream.
+        Task CopyToAsync(Stream target, CancellationToken cancellationToken = default);
+
+        // Opens the request stream for reading the uploaded file.
+        Stream OpenReadStream();
+    }
+}
