428e8e397d8beebddeaedd365a5afea5a3942595
hmarcone <hmarcone@gmail.com>
2019-04-29 10:26:58 -0300

    Add files via upload

---
+++ Reply.UpLoad/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reply.UpLoad.Models;
+
+namespace Reply.UpLoad.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UpLoadImage> UpLoadImages { get; set; }
+    }
+}
