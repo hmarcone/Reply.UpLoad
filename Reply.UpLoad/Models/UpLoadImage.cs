9efa99f98dd0be591d1cc10ce4ea220ef54c1462
hmarcone <hmarcone@gmail.com>
2019-04-29 10:29:18 -0300

    Add files via upload

---
+++ Reply.UpLoad/Models/UpLoadImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reply.UpLoad.Models
+{
+    public class UpLoadImage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+    }
+}
