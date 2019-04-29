6f883cfbcbce7533d6cbeda38e7ff8e65ef9508a
hmarcone <hmarcone@gmail.com>
2019-04-29 10:31:36 -0300

    Add files via upload

---
+++ Reply.UpLoad/ViewModels/UpLoadImageVM.cs
@@ -0,0 +1,12 @@
+﻿using Reply.UpLoad.Interfaces;
+
+namespace Reply.UpLoad.Data
+{
+    public class UpLoadImageVM
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+
+    }
+}
